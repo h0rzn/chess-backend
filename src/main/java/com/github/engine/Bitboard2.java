d2bb0c223077425b4dd92f2a96533d9929126b03
theo <hrzn@protonmail.com>
2023-10-22 23:54:17 +0200

    added initial position generation

--- src/main/java/com/github/engine/Bitboard2.java
+++ src/main/java/com/github/engine/Bitboard2.java
@@ -1,5 +1,6 @@
 package com.github.engine;
 
+import java.math.BigInteger;
 import java.util.BitSet;
 
 public class Bitboard2 {
@@ -50,10 +51,60 @@ public class Bitboard2 {
         return -1;
     }
 
+    // Just for testing around
+    public void Print64() {
+        long boardAll = 0L;
+
+        for (long board : this.boardWhite) {
+            boardAll = boardAll | board;
+        }
+        for (long board : this.boardBlack) {
+            boardAll = boardAll | board;
+        }
+
+        String binStr = Long.toBinaryString((long)boardAll);
+
+        String bin = String.format("%064d%n", new BigInteger(binStr));
+
+        String[] split = bin.split("(?<=\\G.{" + 8 + "})");
+        for (String row : split) {
+            System.out.println(row);
+        }
+    }
+
     public Bitboard2() {
         this.boardWhite = new long[6];
         this.boardBlack = new long[6];
 
         // generate starting positions
+        // PAWNS 0
+        long pawnsW = 0b11111111 << 8;
+        this.boardWhite[0] = this.boardWhite[0] | pawnsW;
+        this.boardBlack[0] = this.boardBlack[0] | pawnsW << 40;
+
+        // KNIGHTS 1
+        long knightsW = 0b01000010;
+        this.boardWhite[1] = this.boardWhite[1] | knightsW;
+        this.boardBlack[1] = this.boardBlack[1] | knightsW << 56;
+
+        // BISHOPS 2
+        long bishopsW = 0b00100100;
+        this.boardWhite[2] = this.boardWhite[2] | bishopsW;
+        this.boardBlack[2] = this.boardBlack[2] | bishopsW << 56;
+
+        // ROOKS 3
+        long rooksW = 0b10000001;
+        this.boardWhite[3] = this.boardWhite[3] | rooksW;
+        this.boardBlack[3] = this.boardBlack[3] | rooksW << 56;
+
+        // QUEENS 4
+        long queensW = 0b00010000;
+        this.boardWhite[4] = this.boardWhite[4] | queensW;
+        this.boardBlack[4] = this.boardBlack[4] | (queensW << 56);
+
+        // KINGS 5
+        long kingsW = 0b0001000;
+        this.boardBlack[4] = this.boardBlack[4] | kingsW;
+        this.boardBlack[4] = this.boardBlack[4] | (kingsW << 56);
     }
 }
