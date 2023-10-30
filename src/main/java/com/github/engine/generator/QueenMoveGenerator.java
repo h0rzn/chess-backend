9d42d3633062b0e414c46fd4aa910e0a4b6a9fbb
Theo Selpin <hrzn@protonmail.com>
2023-10-30 20:41:09 +0100

    queen move generation: added check-cursor generation

--- src/main/java/com/github/engine/generator/QueenMoveGenerator.java
+++ src/main/java/com/github/engine/generator/QueenMoveGenerator.java
@@ -31,9 +31,52 @@ public class QueenMoveGenerator implements IBoard, IGenerator {
         long ownPieces = (ownBoard[0] | ownBoard[1] | ownBoard[2] | ownBoard[3] | ownBoard[5]);
 
         int queenIndex = move.getFrom().getIndex();
-        long queenPosition = 1L << queenIndex;
+        long cursor = 1L << queenIndex;
+        long northCursor = cursor << 8;
+        long southCursor = cursor >> 8;
+        long eastCursor = cursor << 1;
+        long westCursor = cursor >> 1;
+        long northEastCursor = cursor << 9;
+        long northWestCursor = cursor << 7;
+        long southEastCursor = cursor >> 7;
+        long southWestCursor = cursor >> 9;
 
-        // TODO Create possible moves
+        int maxSouth = queenIndex / 8;
+        int maxNorth = 8 - maxSouth - 1;
+        int maxWest = queenIndex % 8;
+        int maxEast = 8 - maxWest - 1;
+        int maxNorthEast = Math.min(maxNorth, maxEast);
+        int maxNorthWest = Math.min(maxNorth, maxWest);
+        int maxSouthEast = Math.min(maxSouth, maxEast);
+        int maxSouthWest = Math.min(maxSouth, maxWest);
+
+        for (int i = 0; i < 8; i++) {
+            if (i < maxNorth) {
+                northCursor <<= 8;
+            }
+            if (i < maxSouth) {
+                southCursor >>= 8;
+            }
+            if (i < maxEast) {
+                eastCursor >>= 8;
+            }
+            if (i < maxWest) {
+                westCursor >>= 8;
+            }
+
+            if (i < maxNorthEast) {
+                northEastCursor <<= 9;
+            }
+            if (i < maxNorthWest) {
+                northWestCursor <<= 7;
+            }
+            if (i < maxSouthEast) {
+                southEastCursor >>= 7;
+            }
+            if (i < maxSouthWest) {
+                southWestCursor >>= 9;
+            }
+        }
 
 
 
