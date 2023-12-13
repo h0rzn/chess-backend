97f837a9b33eb25bc3e4c36208c271956f208899
Theo Selpin <hrzn@protonmail.com>
2023-12-13 19:40:46 +0100

    added pieceType & moveType to Move class

--- src/main/java/com/github/engine/move/Move.java
+++ src/main/java/com/github/engine/move/Move.java
@@ -1,13 +1,30 @@
 package com.github.engine.move;
 
 import lombok.Getter;
+import lombok.Setter;
 
 public class Move {
     @Getter
     private Position from;
     @Getter
     private Position to;
+    // pieceType is not passed to the constructor
+    // but rather set during move processing
+    @Getter
+    @Setter
+    private int pieceType;
+    // moveType is - just as pieceType - set during
+    // move processing
+    @Getter
+    @Setter
+    private int moveType;
 
+    public Move(int from, int to) {
+        this.from = new Position(from);
+        this.to = new Position(to);
+        this.pieceType = -1;
+        this.moveType = -1;
+    }
 
     public Move(String move) {
         if (move.length() != 5) {
@@ -16,7 +33,7 @@ public class Move {
         this.from = new Position(move.split("-")[0]);
         this.to = new Position(move.split("-")[1]);
     }
-
+    /*
     public Move(Position from, Position to) {
         this.from = from;
         this.to = to;
@@ -36,5 +53,5 @@ public class Move {
         this.from = new Position(fromRank, fromFile, index);
         this.to = new Position(toRank, toFile, index);
     }
-
+    */
 }
